31a30fcdd97595853e0b167c41e63e1369af0102
Mikkaiser <mikaelrsimoes19@gmail.com>
2022-04-11 09:50:34 -0300

    add: .env

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,15 +1,15 @@
 import "reflect-metadata"
-import { DataSource } from "typeorm"
+import { DatabaseType, DataSource, DataSourceOptions } from "typeorm";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "123",
-    database: "db_trip_habitat",
-    synchronize: true,
-    logging: false,
-    entities: ['src/entities/**/*.ts'],
-    migrations: ['src/database/migrations/**/*.ts*.js'],
-})
+    type: `${process.env.DB_TYPE}` as any,
+    host: `${process.env.DB_HOST}`,
+    port: Number(process.env.DB_PORT),
+    username: `${process.env.DB_USERNAME}`,
+    password: `${process.env.DB_PASSWORD}`,
+    database: `${process.env.DB_NAME}`,
+    synchronize: Boolean(process.env.DB_SYNCHRONIZE),
+    logging: Boolean(process.env.DB_LOGGING),
+    entities: [`${process.env.ENTITIES_DIR}`],
+    migrations: [`${process.env.MIGRATIONS_DIR}`],
+});
